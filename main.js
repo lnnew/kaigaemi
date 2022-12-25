6e03b20dc3493a4fe7784da797a266230e7d1b51
unknown <yoojihyun6933@kaist.ac.kr>
2022-12-26 08:14:44 +0900

    b

--- main.js
+++ main.js
@@ -56,6 +56,6 @@ app.use(function (err, req, res, next) {
   res.status(500).send('Something broke!')
 });
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log('Example app listening on port 3000!')
 });
