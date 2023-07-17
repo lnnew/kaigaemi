0869cfe67a755adf48c9dade752317a20d68b7a7
lnnew <121406686+lnnew@users.noreply.github.com>
2023-07-17 11:57:09 +0900

    Update main.js

--- main.js
+++ main.js
@@ -61,6 +61,6 @@ app.use(function (err, req, res, next) {
   res.status(500).send('Something broke!')
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Example app listening on port d!')
 });
